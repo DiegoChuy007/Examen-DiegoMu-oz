3b4432caf5a8be4ca0c526f053a52087bbc5afc7
DiegoChuy007 <diegmg04@gmail.com>
2025-09-18 11:51:48 -0600

    Nueva versión

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from clases.areas import areas
 
 def main():
-    ejemplo=areas(4,8)
+    ejemplo=areas(4,8,6)
     print("El área del triángulo es: ", ejemplo.area_triangulo())
     print("El área del rectángulo es: ", ejemplo.area_rectangulo())
+    print("El área del círculo es: ", ejemplo.area_circulo())
 
 if __name__ == '__main__':
     main()    
